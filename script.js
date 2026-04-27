4c28952292ceeb2b09e9e3ba48f8e226010cd7ef
Diana Dorobantu <dianamihaela.dorobantu@gmail.com>
2026-04-27 11:00:49 +0100

    add dark/light toggle

--- script.js
+++ script.js
@@ -22,3 +22,21 @@ sections.forEach(section => {
     section.style.transform = "translateY(40px)";
     section.style.transition = "all 0.6s ease";
 });
+
+
+const toggleBtn = document.getElementById("theme-toggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light-mode");
+    toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    const isLight = document.body.classList.contains("light-mode");
+
+    toggleBtn.textContent = isLight ? "☀️" : "🌙";
+    localStorage.setItem("theme", isLight ? "light" : "dark");
+});
